6e0d784b385753e25e0124c8dc5f54d889f566cc
Kalvin Lam <kalvin.lam5@gmail.com>
2018-04-14 20:40:47 -0400

    Got music to play when hovering over Nono Boy

--- assets/js/music-functionality.js
+++ assets/js/music-functionality.js
@@ -1,5 +1,15 @@
+window.onload = function() {
+    if (window.jQuery) {
+        // jQuery is loaded
+        console.log("jQuery is loaded! and music-functionality.js is recognized!");
+    } else {
+        // jQuery is not loaded
+        alert("jQuery Doesn't Work");
+    }
+}
+
 var audio = $("#mySoundClip")[0];
-$(".item thumb").mouseenter(function() {
-  console.log("hover registered")
+$(".inner").mouseenter(function() {
+  alert("hover registered")
   audio.play();
 });
